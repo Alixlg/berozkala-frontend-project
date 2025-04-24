2170818e087223b225a8180c0597ad8be872573f
Alireza <alirezatorkaman.kaman@gmail.com>
2025-04-25 01:27:07 +0330

    add admin panel

---
+++ src/app/+pages/+private/+admin-panel/manage-products/remove-product/ui/remove-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-product',
+  imports: [],
+  templateUrl: './remove-product.component.html',
+  styleUrl: './remove-product.component.css'
+})
+export class RemoveProductComponent {
+
+}
